37b75acffd6e8210903edf3ed298ff65bee9c3b3
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-12-09 22:59:12 +0000

    tax program

--- Assign-02-Python/Assign-02-Python.py
+++ Assign-02-Python/Assign-02-Python.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # Created by: Lia Duggan
 # Created on: Oct. 1st, 2022
@@ -8,6 +7,7 @@
 import math
 
 # Input
+print("\033[1;36;40m \n")
 side_len = float(input("What is the side length of the pentagon? = "))
 
 # Process
@@ -16,5 +16,6 @@ pentagon_area = (math.sqrt(5 * (5 + 2 * math.sqrt(5))) * pow(side_len, 2)) / 4.0
 pentagon_perimeter = 5 * side_len
 
 # output
+print("\033[1;36;40m \n")
 print("The pentagon's area is ", pentagon_area)
 print("and the pentagon's perimeter is ", pentagon_perimeter)
